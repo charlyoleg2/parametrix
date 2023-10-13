1ca7cd36fed18156d1857a1980f2330b5efa152f
charles <charles.braquet@e.email>
2023-10-13 21:35:33 +0200

    symbole for axis

--- pkg/parametrix/src/routes/docs/axis/+page.svelte
+++ pkg/parametrix/src/routes/docs/axis/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	/* eslint-disable svelte/no-at-html-tags */
 	import { base } from '$app/paths';
+	import { math } from 'mathlifier';
 
 	// inputs
 	let load_mass = 600; // kg
@@ -104,7 +106,15 @@
 	<img src="{base}/epicyclic_gearing.svg" alt="epicyclic gearing" />
 </article>
 <h2>Theory</h2>
-<article></article>
+<article>
+	<ul class="formula">
+		<li>{@html math('T_1 = 9.81 * M * D_1')}</li>
+		<li>{@html math('T_2 = T_1 * F_s')}</li>
+		<li>{@html math('t_{ot} = 2 * t_{ht}')}</li>
+		<li>{@html math('\\omega_{axis} = \\frac{60}{t_{ot}}')}</li>
+		<li>{@html math('P_{axis} = \\frac{T_2 * 2 * \\pi}{t_{ot}}')}</li>
+	</ul>
+</article>
 <h2>Numerical application</h2>
 <article>
 	<table>
@@ -133,39 +143,39 @@
 			<td><input type="range" bind:value={d1} min="0.1" max="20" step="0.1" /></td>
 		</tr>
 		<tr>
-			<td>T</td>
+			<td>{@html math('T_1')}</td>
 			<td>Load torque (N.m)</td>
 			<td>{torque1.toFixed(2)} N.m</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('F_s')}</td>
 			<td>Security factor</td>
 			<td><input type="number" bind:value={securityFactor} min="1" max="10" step="0.1" /></td>
 			<td><input type="range" bind:value={securityFactor} min="1" max="10" step="0.1" /></td>
 		</tr>
 		<tr>
-			<td>T</td>
+			<td>{@html math('T_2')}</td>
 			<td>Given torque (N.m)</td>
 			<td>{torque2.toFixed(2)} N.m</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('t_{ht}')}</td>
 			<td>Time for half turn (s)</td>
 			<td><input type="number" bind:value={halfTurn} min="1" max="720" step="1" /></td>
 			<td><input type="range" bind:value={halfTurn} min="1" max="720" step="1" /></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('t_{ot}')}</td>
 			<td>Time for one turn (s)</td>
 			<td>{oneTurn.toFixed(2)} s</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('\\omega_{axis}')}</td>
 			<td>Axis rotation speed (rpm)</td>
 			<td>{(60 / oneTurn).toFixed(2)} rpm</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('P_{axis}')}</td>
 			<td>Power at axis (W)</td>
 			<td>{power2.toFixed(2)} W</td>
 		</tr>
@@ -176,79 +186,79 @@
 			<td></td>
 		</tr>
 		<tr>
-			<td>N1</td>
+			<td>{@html math('N_r')}</td>
 			<td>Number of gear-rings</td>
 			<td><input type="number" bind:value={ringNb} min="1" max="4" step="1" /></td>
 			<td><input type="range" bind:value={ringNb} min="1" max="4" step="1" /></td>
 		</tr>
 		<tr>
-			<td>N2</td>
+			<td>{@html math('N_w')}</td>
 			<td>Number of gear-wheels</td>
 			<td><input type="number" bind:value={wheelNb} min="1" max="24" step="1" /></td>
 			<td><input type="range" bind:value={wheelNb} min="1" max="24" step="1" /></td>
 		</tr>
 		<tr>
-			<td>mw</td>
+			<td>{@html math('m_w')}</td>
 			<td>Module of gear-wheel (mm)</td>
 			<td><input type="number" bind:value={wheelModule} min="1" max="100" step="0.1" /></td>
 			<td><input type="range" bind:value={wheelModule} min="1" max="100" step="0.1" /></td>
 		</tr>
 		<tr>
-			<td>Zw</td>
+			<td>{@html math('Z_w')}</td>
 			<td>Number of teeth of gear-wheel</td>
 			<td><input type="number" bind:value={wheelZ} min="17" max="40" step="1" /></td>
 			<td><input type="range" bind:value={wheelZ} min="17" max="40" step="1" /></td>
 		</tr>
 		<tr>
-			<td>Dw</td>
+			<td>{@html math('D_w')}</td>
 			<td>Diameter of gear-wheel (mm)</td>
 			<td>{wheelDiameter.toFixed(2)} mm</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('Margin_w')}</td>
 			<td>Margin between gear-wheels (%)</td>
 			<td><input type="number" bind:value={wheelMargin} min="0.1" max="20" step="0.1" /></td>
 			<td><input type="range" bind:value={wheelMargin} min="0.1" max="20" step="0.1" /></td>
 		</tr>
 		<tr>
-			<td>Dr</td>
+			<td>{@html math('D_r')}</td>
 			<td>Diameter of gear-ring (mm)</td>
 			<td>{ringDiameter.toFixed(2)} mm</td>
 		</tr>
 		<tr>
-			<td>Zr</td>
+			<td>{@html math('Z_r')}</td>
 			<td>Number of teeth of gear-ring</td>
 			<td>{ringZ}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('R_{r/w}')}</td>
 			<td>Ratio ring / gear-wheel</td>
 			<td>1 : {ratioRW.toFixed(2)}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('E')}</td>
 			<td>Gear efficiency (%)</td>
 			<td><input type="number" bind:value={gearEffi} min="1" max="100" step="0.5" /></td>
 			<td><input type="range" bind:value={gearEffi} min="1" max="100" step="0.5" /></td>
 		</tr>
 		<tr>
-			<td>Tw</td>
+			<td>{@html math('T_3')}</td>
 			<td>Torque of gear-wheel (N.m)</td>
 			<td>{torque3.toFixed(2)} N.m</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('t_3')}</td>
 			<td>Gear-wheel rotation time (s)</td>
 			<td>{speed3.toFixed(2)} s</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('s_w')}</td>
 			<td>Gear-wheel rotation speed (rpm)</td>
 			<td>{(60 / speed3).toFixed(2)} rpm</td>
 		</tr>
 		<tr>
-			<td></td>
-			<td>Power at gearwheel (W)</td>
+			<td>{@html math('P_w')}</td>
+			<td>Power at gear-wheel (W)</td>
 			<td>{power3.toFixed(2)} W</td>
 		</tr>
 		<tr class="subtitle">
@@ -258,81 +268,81 @@
 			<td></td>
 		</tr>
 		<tr>
-			<td>Nr1</td>
+			<td>{@html math('N_{r1p}')}</td>
 			<td>Number of planets of reductor-1</td>
 			<td><input type="number" bind:value={planet1Nb} min="1" max="24" step="1" /></td>
 			<td><input type="range" bind:value={planet1Nb} min="1" max="24" step="1" /></td>
 		</tr>
 		<tr>
-			<td>mr1</td>
+			<td>{@html math('m_{r1}')}</td>
 			<td>Module of epicyclic of reductor-1 (mm)</td>
 			<td><input type="number" bind:value={epic1Module} min="1" max="100" step="0.1" /></td>
 			<td><input type="range" bind:value={epic1Module} min="1" max="100" step="0.1" /></td>
 		</tr>
 		<tr>
-			<td>Zr1</td>
+			<td>{@html math('Z_{r1p}')}</td>
 			<td>Number of teeth of planet-1</td>
 			<td><input type="number" bind:value={planet1Z} min="17" max="40" step="1" /></td>
 			<td><input type="range" bind:value={planet1Z} min="17" max="40" step="1" /></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('D_{r1p}')}</td>
 			<td>Diameter of planet-1 (mm)</td>
 			<td>{planet1Diameter.toFixed(2)} mm</td>
 		</tr>
 		<tr>
-			<td>Dr1</td>
+			<td>{@html math('D_{r1r}')}</td>
 			<td>Diameter of ring of reductor-1 (mm)</td>
 			<td>{ring1Diameter.toFixed(2)} mm</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('Z_{r1r}')}</td>
 			<td>Number of teeth of ring-1</td>
 			<td>{ring1Z}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('Z_{r1s}')}</td>
 			<td>Number of teeth of sun-1</td>
 			<td>{sun1Z}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('R_{r1-1}')}</td>
 			<td>Ratio of one stage of reductor-1</td>
 			<td>1 : {ratio1One.toFixed(2)}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('N_{r1}')}</td>
 			<td>Number of stages of reductor-1</td>
 			<td><input type="number" bind:value={stage1Nb} min="1" max="10" step="1" /></td>
 			<td><input type="range" bind:value={stage1Nb} min="1" max="10" step="1" /></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('R_{r1}')}</td>
 			<td>Ratio of reductor-1</td>
 			<td>1 : {ratio1All.toFixed(2)}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('E_{r1}')}</td>
 			<td>Efficiency of reductor-1</td>
 			<td>{(100 * (gearEffi / 100) ** stage1Nb).toFixed(2)} %</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('T_4')}</td>
 			<td>Reductor-1 input torque (N.m)</td>
 			<td>{torque4.toFixed(2)} N.m</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('f_4')}</td>
 			<td>Reductor-1 input rotation speed (Hz)</td>
 			<td>{speed4.toFixed(2)} Hz</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('s_4')}</td>
 			<td>Reductor-1 input rotation speed (rpm)</td>
 			<td>{(60 * speed4).toFixed(2)} rpm</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('P_{r1}')}</td>
 			<td>Power at reductor-1 input (W)</td>
 			<td>{power4.toFixed(2)} W</td>
 		</tr>
@@ -343,12 +353,12 @@
 			<td></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('R_{a/r1i}')}</td>
 			<td>Ratio from reductor-1 input to axis</td>
 			<td>1 : {(ratioRW * ratio1All).toFixed(2)}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('E_{a/r1i}')}</td>
 			<td>Efficiency from reductor-1 input to axis</td>
 			<td>{(100 * (gearEffi / 100) ** (1 + stage1Nb)).toFixed(2)} %</td>
 		</tr>
@@ -359,81 +369,81 @@
 			<td></td>
 		</tr>
 		<tr>
-			<td>Nr2</td>
+			<td>{@html math('N_{r2p}')}</td>
 			<td>Number of planets of reductor-2</td>
 			<td><input type="number" bind:value={planet2Nb} min="1" max="24" step="1" /></td>
 			<td><input type="range" bind:value={planet2Nb} min="1" max="24" step="1" /></td>
 		</tr>
 		<tr>
-			<td>mr2</td>
+			<td>{@html math('m_{r2}')}</td>
 			<td>Module of epicyclic of reductor-2 (mm)</td>
 			<td><input type="number" bind:value={epic2Module} min="1" max="100" step="0.1" /></td>
 			<td><input type="range" bind:value={epic2Module} min="1" max="100" step="0.1" /></td>
 		</tr>
 		<tr>
-			<td>Zr2</td>
+			<td>{@html math('Z_{r2p}')}</td>
 			<td>Number of teeth of planet-2</td>
 			<td><input type="number" bind:value={planet2Z} min="17" max="40" step="1" /></td>
 			<td><input type="range" bind:value={planet2Z} min="17" max="40" step="1" /></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('D_{r2p}')}</td>
 			<td>Diameter of planet-2 (mm)</td>
 			<td>{planet2Diameter.toFixed(2)} mm</td>
 		</tr>
 		<tr>
-			<td>Dr2</td>
+			<td>{@html math('D_{r2r}')}</td>
 			<td>Diameter of ring of reductor-2 (mm)</td>
 			<td>{ring2Diameter.toFixed(2)} mm</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('Z_{r2r}')}</td>
 			<td>Number of teeth of ring-2</td>
 			<td>{ring2Z}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('Z_{r2s}')}</td>
 			<td>Number of teeth of sun-2</td>
 			<td>{sun2Z}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('R_{r2}')}</td>
 			<td>Ratio of one stage of reductor-2</td>
 			<td>1 : {ratio2One.toFixed(2)}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('N_{r2-1}')}</td>
 			<td>Number of stages of reductor-2</td>
 			<td><input type="number" bind:value={stage2Nb} min="1" max="10" step="1" /></td>
 			<td><input type="range" bind:value={stage2Nb} min="1" max="10" step="1" /></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('N_{r2}')}</td>
 			<td>Ratio of reductor-2</td>
 			<td>1 : {ratio2All.toFixed(2)}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('E_{r2}')}</td>
 			<td>Efficiency of reductor-2</td>
 			<td>{(100 * (gearEffi / 100) ** stage2Nb).toFixed(2)} %</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('T_5')}</td>
 			<td>Reductor-2 input torque (N.m)</td>
 			<td>{torque5.toFixed(2)} N.m</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('f_{r2i}')}</td>
 			<td>Reductor-2 input rotation speed (Hz)</td>
 			<td>{speed5.toFixed(2)} Hz</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('s_{r2i}')}</td>
 			<td>Reductor-2 input rotation speed (rpm)</td>
 			<td>{(60 * speed5).toFixed(2)} rpm</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('P_5')}</td>
 			<td>Power at reductor-2 input (W)</td>
 			<td>{power5.toFixed(2)} W</td>
 		</tr>
@@ -444,12 +454,12 @@
 			<td></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('R_{a/r2i}')}</td>
 			<td>Ratio from reductor-2 input to axis</td>
 			<td>1 : {(ratioRW * ratio1All * ratio2All).toFixed(2)}</td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('E_{a/r2i}')}</td>
 			<td>Efficiency from reductor-2 input to axis</td>
 			<td>{(100 * (gearEffi / 100) ** (1 + stage1Nb + stage2Nb)).toFixed(2)} %</td>
 		</tr>
@@ -460,13 +470,13 @@
 			<td></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('F_s')}</td>
 			<td>Force on one shuttle (N)</td>
 			<td><input type="number" bind:value={shutFor} min="0.0001" max="2" step="0.0001" /></td>
 			<td><input type="range" bind:value={shutFor} min="0.0001" max="2" step="0.0001" /></td>
 		</tr>
 		<tr>
-			<td></td>
+			<td>{@html math('D_s')}</td>
 			<td>Shuttle diameter (mm)</td>
 			<td>{((1000 * torque5) / shutFor).toFixed(2)} mm</td>
 		</tr>
@@ -488,6 +498,16 @@
 	article {
 		margin: 1rem;
 		margin-top: 0.2rem;
+	}
+	ul {
+		margin-top: 0.2rem;
+		margin-bottom: 0.2rem;
+		margin-left: 0.2rem;
+		padding-left: 1rem;
+	}
+	ul.formula > li {
+		padding-top: 0.3rem;
+		padding-bottom: 0.3rem;
 	}
 	article > table > tr.subtitle {
 		background-color: colors.$table-head;
