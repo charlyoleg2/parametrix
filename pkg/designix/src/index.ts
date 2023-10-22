efcbe8b26ec2e52e7ea33005efd0cfd98c5febc9
charles <charles.braquet@e.email>
2023-10-22 21:02:55 +0200

    tidy up designix with directories

--- pkg/designix/src/index.ts
+++ pkg/designix/src/index.ts
@@ -1,21 +1,21 @@
 // index.ts : entry point
 
-export * from './gear_wheel_wheel';
-export * from './simplified_gear_wheel';
+export * from './gears/gear_wheel_wheel';
+export * from './gears/simplified_gear_wheel';
 
-export * from './pole_static';
+export * from './heliostat/pole_static';
 
-export * from './circles';
-export * from './rectangle';
+export * from './dummies/circles';
+export * from './dummies/rectangle';
 
-export * from './verify_point';
-export * from './verify_point_2';
-export * from './verify_line';
-export * from './verify_line_2';
-export * from './verify_line_3';
-export * from './verify_vector';
-export * from './verify_contour_1';
-export * from './verify_contour_2';
-export * from './verify_contour_3';
-export * from './verify_contour_4';
-export * from './verify_exports_1';
+export * from './geometrix_verification/verify_point';
+export * from './geometrix_verification/verify_point_2';
+export * from './geometrix_verification/verify_line';
+export * from './geometrix_verification/verify_line_2';
+export * from './geometrix_verification/verify_line_3';
+export * from './geometrix_verification/verify_vector';
+export * from './geometrix_verification/verify_contour_1';
+export * from './geometrix_verification/verify_contour_2';
+export * from './geometrix_verification/verify_contour_3';
+export * from './geometrix_verification/verify_contour_4';
+export * from './geometrix_verification/verify_exports_1';
