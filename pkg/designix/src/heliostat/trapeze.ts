3975523a7905130fccdd53b9a26ed6957c8a493f
charles <charles.braquet@e.email>
2023-11-01 22:51:52 +0100

    3D rods

--- pkg/designix/src/heliostat/trapeze.ts
+++ pkg/designix/src/heliostat/trapeze.ts
@@ -92,6 +92,7 @@ function pGeom(t: number, param: tParamVal): tGeom {
 	const figPlate = figure();
 	const figRod = figure();
 	const figRodHollow = figure();
+	const figCutRod = figure();
 	rGeome.logstr += `simTime: ${t}\n`;
 	try {
 		if (param.L3 > param.L1 - param.L5) {
@@ -283,17 +284,32 @@ function pGeom(t: number, param: tParamVal): tGeom {
 		if (param.D4 >= param.D3) {
 			throw `err218: D4 ${param.D4} larger than D3 ${param.D3}`;
 		}
-		figRod.addMain(contourCircle(0, 0, param.D3));
-		figRod.addSecond(contourCircle(0, 0, param.D4));
+		figRod.addMain(contourCircle(0, 0, param.D3 / 2));
+		figRod.addSecond(contourCircle(0, 0, param.D4 / 2));
 		// figRodHollow
-		figRodHollow.addMain(contourCircle(0, 0, param.D4));
-		figRodHollow.addSecond(contourCircle(0, 0, param.D3));
+		figRodHollow.addMain(contourCircle(0, 0, param.D4 / 2));
+		figRodHollow.addSecond(contourCircle(0, 0, param.D3 / 2));
+		// figCutRod
+		const rodFootprintHeight = param.D3 * Math.cos(rod_slope_angle);
+		const rodFootprintBack = rodFootprintHeight / Math.tan(rod_slope_angle);
+		const pad3x = pad3 + (rodFootprintLength / 2 + rodFootprintBack) * Math.cos(rod_xy_angle);
+		const pad3y = pad3 + (rodFootprintLength / 2 + rodFootprintBack) * Math.sin(rod_xy_angle);
+		const rodExtrudeLength =
+			rod_slope_length + 2 * (rodFootprintLength + rodFootprintBack + rodFootprintHeight);
+		const cutL = rodExtrudeLength + param.L3 + param.L4 + rodFootprintHeight;
+		const ctrCutRod = contour(cutL, cutL)
+			.addSegStrokeA(-cutL, cutL)
+			.addSegStrokeA(-cutL, -cutL)
+			.addSegStrokeA(cutL, -cutL)
+			.closeSegStroke();
+		figCutRod.addMain(ctrCutRod);
 		// final figure list
 		rGeome.fig = {
 			faceFrame: figFrame,
 			facePlate: figPlate,
 			faceRod: figRod,
-			faceRodHollow: figRodHollow
+			faceRodHollow: figRodHollow,
+			faceCutRod: figCutRod
 		};
 		const designName = pDef.partName;
 		rGeome.vol = {
@@ -313,13 +329,101 @@ function pGeom(t: number, param: tParamVal): tGeom {
 					length: param.H3,
 					rotate: [0, 0, 0],
 					translate: [0, 0, 0]
+				},
+				{
+					outName: `subpax_${designName}_rod1`,
+					face: `${designName}_faceRod`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: rodExtrudeLength,
+					rotate: [0, Math.PI / 2 - rod_slope_angle, rod_xy_angle],
+					translate: [
+						param.L4 / 2 - pad3x,
+						param.L3 / 2 - pad3y,
+						param.H3 - rodFootprintHeight
+					]
+				},
+				{
+					outName: `subpax_${designName}_rod2`,
+					face: `${designName}_faceRod`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: rodExtrudeLength,
+					rotate: [0, Math.PI / 2 - rod_slope_angle, Math.PI - rod_xy_angle],
+					translate: [
+						-param.L4 / 2 + pad3x,
+						param.L3 / 2 - pad3y,
+						param.H3 - rodFootprintHeight
+					]
+				},
+				{
+					outName: `subpax_${designName}_rod3`,
+					face: `${designName}_faceRod`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: rodExtrudeLength,
+					rotate: [0, Math.PI / 2 - rod_slope_angle, Math.PI + rod_xy_angle],
+					translate: [
+						-param.L4 / 2 + pad3x,
+						-param.L3 / 2 + pad3y,
+						param.H3 - rodFootprintHeight
+					]
+				},
+				{
+					outName: `subpax_${designName}_rod4`,
+					face: `${designName}_faceRod`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: rodExtrudeLength,
+					rotate: [0, Math.PI / 2 - rod_slope_angle, -rod_xy_angle],
+					translate: [
+						param.L4 / 2 - pad3x,
+						-param.L3 / 2 + pad3y,
+						param.H3 - rodFootprintHeight
+					]
+				},
+				{
+					outName: `subpax_${designName}_cut1`,
+					face: `${designName}_faceCutRod`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: cutL,
+					rotate: [0, 0, 0],
+					translate: [0, 0, -cutL]
+				},
+				{
+					outName: `subpax_${designName}_cut2`,
+					face: `${designName}_faceCutRod`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: cutL,
+					rotate: [0, 0, 0],
+					translate: [0, 0, param.H1]
 				}
 			],
 			volumes: [
 				{
+					outName: `ipax_${designName}_rawRod`,
+					boolMethod: EBVolume.eUnion,
+					inList: [
+						`subpax_${designName}_rod1`,
+						`subpax_${designName}_rod2`,
+						`subpax_${designName}_rod3`,
+						`subpax_${designName}_rod4`
+					]
+				},
+				{
+					outName: `ipax_${designName}_halfRods`,
+					boolMethod: EBVolume.eSubstraction,
+					inList: [`ipax_${designName}_rawRod`, `subpax_${designName}_cut1`]
+				},
+				{
+					outName: `ipax_${designName}_rods`,
+					boolMethod: EBVolume.eSubstraction,
+					inList: [`ipax_${designName}_halfRods`, `subpax_${designName}_cut2`]
+				},
+				{
 					outName: `pax_${designName}`,
 					boolMethod: EBVolume.eUnion,
-					inList: [`subpax_${designName}_frame`, `subpax_${designName}_plate`]
+					inList: [
+						`subpax_${designName}_frame`,
+						`subpax_${designName}_plate`,
+						`ipax_${designName}_rods`
+					]
 				}
 			]
 		};
