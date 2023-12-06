702432e1835dbbf0c96d638cb069e64143dfdbd4
charles <charles.braquet@e.email>
2023-12-06 04:40:45 +0100

    complete makeMenu

--- pkg/paramui/src/lib/Navigat.svelte
+++ pkg/paramui/src/lib/Navigat.svelte
@@ -2,13 +2,13 @@
 	//import type { HTMLAttributes } from 'svelte/elements';
 	//type $$Props = HTMLAttributes<HTMLElement>;
 
-	import { checkEmptyPath, listOneCategory } from './makeMenu';
+	import { checkPath, listOneCategory } from './makeMenu';
 	import Navig from './Navig.svelte';
 
 	import { page } from '$app/stores';
 	import { base } from '$app/paths';
 	let pagePath: string;
-	$: pagePath = checkEmptyPath($page.url.pathname.replace(base, ''));
+	$: pagePath = checkPath($page.url.pathname.replace(base, ''));
 </script>
 
 <Navig menuList={listOneCategory(pagePath)} menuSelected={pagePath} />
--- pkg/paramui/src/lib/makeMenu.ts
+++ pkg/paramui/src/lib/makeMenu.ts
@@ -7,7 +7,10 @@ import { menuList } from './menuList';
 
 function pathToLabel(ipath: string): string {
 	const re = /^.*\//g;
-	const rLabel = ipath.replace(re, '');
+	let rLabel = ipath.replace(re, '');
+	if (rLabel === '') {
+		rLabel = 'index';
+	}
 	return rLabel;
 }
 
@@ -43,6 +46,15 @@ function makeDesignDefs(iMenu: tMenuList): tAllPageDef {
 	return rDesignDefs;
 }
 
+function checkIndexPath(iPath: string): string {
+	//console.log(`dbg518: ${iPath}`);
+	let rpath = iPath;
+	if ((rpath === '/index') || (rpath === '/index.html')) {
+		rpath = '/';
+		//console.log(`dbg565: ${rpath}`);
+	}
+	return rpath;
+}
 function checkEmptyPath(iPath: string): string {
 	let rPath = iPath;
 	if (rPath === '') {
@@ -50,26 +62,46 @@ function checkEmptyPath(iPath: string): string {
 	}
 	return rPath;
 }
+function checkPath(iPath: string): string {
+	let rPath = iPath;
+	rPath = checkEmptyPath(rPath);
+	rPath = checkIndexPath(rPath);
+	return rPath;
+}
 
+// create contextual-top-menu
+const pageIndex: tPageOne = { path: '/', page: null, svg: 'page_index.svg', label: 'index' };
+const pageAbout: tPageOne = { path: '/about', page: null, svg: 'page_about.svg', label: 'about' };
+const categ0: tCategoryOne = { category: '', pages: [pageIndex, pageAbout] };
 function listOneCategorySub(iMenu: tMenuList, ipath: string): tCategoryOne {
+	//console.log(`dbg572: ${ipath}`);
+	let spath = checkIndexPath(ipath);
 	let categIdx = 0;
+	lookForCategory:
 	for (const [idx, categ] of iMenu.entries()) {
 		for (const pag of categ.pages) {
-			if (ipath === pag.path) {
+			if (spath === pag.path) {
 				categIdx = idx;
+				break lookForCategory;
 			}
 		}
 	}
-	const categ1 = iMenu[categIdx];
-	const rCateg: tCategoryOne = { category: categ1.category, pages: [] };
-	for (const pag of categ1.pages) {
-		const pag2: tPageOne = {
-			path: pag.path,
-			page: pag.page,
-			svg: pag.svg,
-			label: pathToLabel(pag.path)
-		};
-		rCateg.pages.push(pag2);
+	// exception index and about
+	let rCateg = categ0;
+	if ((categIdx > 0) && (categIdx < iMenu.length - 1)) {
+		const categ1 = iMenu[categIdx];
+		rCateg = { category: categ1.category, pages: [] };
+		rCateg.pages.push(pageIndex);
+		for (const pag of categ1.pages) {
+			const pag2: tPageOne = {
+				path: pag.path,
+				page: pag.page,
+				svg: pag.svg,
+				label: pathToLabel(pag.path)
+			};
+			rCateg.pages.push(pag2);
+		}
+		//rCateg.pages.push(pageAbout);
 	}
 	return rCateg;
 }
@@ -82,4 +114,4 @@ function listOneCategory(ipath: string): tCategoryOne {
 const menuList2 = makeMenuList2(menuList);
 const designDefs = makeDesignDefs(menuList);
 
-export { checkEmptyPath, listOneCategory, menuList2, designDefs };
+export { checkPath, listOneCategory, menuList2, designDefs };
