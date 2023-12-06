8b097ee6c934c329475ac2eefebfe91a9ca02feb
charles <charles.braquet@e.email>
2023-12-06 22:03:58 +0100

    starting dev on demo-paramuilib

--- pkg/demo-paramuilib/src/routes/+page.svelte
+++ pkg/demo-paramuilib/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const aaa = 'bbb';
+</script>
+
+<h1>Welcome to demo-paramuilib</h1>
+<h6>
+	Demonstrating the usage of the <a href="https://svelte.dev/">svelte</a> library
+	<strong>paramuilib</strong>
+</h6>
+
+<h2>List of designs:</h2>
+<ul>
+	<li>aaa{aaa}</li>
+</ul>
+
+<style lang="scss">
+	h1 {
+		color: pink;
+	}
+</style>
