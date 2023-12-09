64f310c799763d386de3b1cc2dfa50e5fa80a609
charles <charles.braquet@e.email>
2023-12-09 22:09:30 +0100

    refactor simple_gear_wheel using sub-design features

--- pkg/designix/src/gears/simplified_gear_wheel.ts
+++ pkg/designix/src/gears/simplified_gear_wheel.ts
@@ -11,19 +11,21 @@ import type {
 //import { contour, contourCircle, figure, degToRad } from 'geometrix';
 import {
 	designParam,
-	contour,
+	checkGeom,
+	prefixLog,
+	//contour,
 	figure,
-	degToRad,
-	ffix,
+	//degToRad,
+	//ffix,
 	pNumber,
 	pCheckbox,
 	//pDropdown,
 	initGeom,
-	EExtrude,
+	//EExtrude,
 	EBVolume
 } from 'geometrix';
-import * as gwHelper from './gearWheelProfile';
-import * as welem from './wheelElements';
+//import * as gwHelper from './gearWheelProfile';
+//import * as welem from './wheelElements';
 
 // design import
 import { gearWheelWheelDef } from './gear_wheel_wheel';
@@ -153,211 +155,8 @@ function pGeom(t: number, param: tParamVal): tGeom {
 	const figOne = figure();
 	const figTwo = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
-	const param2: tParamVal = {
-		N2: 23,
-		angleCenterCenter: 0,
-		addInterAxis: 0,
-		ah1: 1,
-		dh1: 1,
-		bh1: 0.25,
-		bRound1: 2,
-		//ah2: 1,
-		//dh2: 1,
-		//bh2: 0.25,
-		//bRound2: 2,
-		at1: 50,
-		//at2: 50,
-		involSym: 1,
-		involROpt: 0,
-		involLOpt: 0,
-		brr1: 200,
-		//brr2: 200,
-		blr1: 200,
-		//blr2: 200,
-		involArcPairs1: 2,
-		skinThickness1: 0,
-		//involArcPairs2: 2,
-		//skinThickness2: 0,
-		initAngle1: 0
-		//gw2Position: 0,
-	};
 	try {
-		// Figure One
-		// re-arrange parameters
-		const gp1 = gwHelper.gwProfile();
-		//const gp2 = gwHelper.gwProfile();
-		gp1.set1ModuleToothNumber(param.module, param.N1);
-		//gp2.set1ModuleToothNumber(param['module'], param['N2']);
-		gp1.set2CenterPosition(param.c1x, param.c1y);
-		const acc = degToRad(param2.angleCenterCenter);
-		//const [c2x, c2y, d12] = gwHelper.gw2center(gp1, gp2, acc, param['addInterAxis']);
-		//gp2.set2CenterPosition(c2x, c2y);
-		gp1.set3CircleRadius(param2.ah1, param2.dh1, param2.bh1, param2.bRound1);
-		//gp2.set3CircleRadius(param['ah2'], param['dh2'], param['bh2'], param['bRound2']);
-		// base circles
-		const brr1 = gp1.dr;
-		const blr1 = gp1.dr;
-		//const [brr1, blr1, brr2, blr2] = gwHelper.baseCircles(
-		//	gp1,
-		//	gp2,
-		//	param['brr1'],
-		//	param['blr1'],
-		//	param['brr2'],
-		//	param['blr2'],
-		//	param['involSym'],
-		//	param['involROpt'],
-		//	param['involROpt']
-		//);
-		gp1.set4BaseCircles(brr1, blr1);
-		//gp2.set4BaseCircles(brr2, blr2);
-		gp1.set5AddendumThickness(param2.at1);
-		//gp2.set5AddendumThickness(param['at2']);
-		const initAngle1 = degToRad(param2.initAngle1) + (t * 3 * gp1.as) / 100; // sim.tMax=100
-		gp1.set6Angles(initAngle1, acc);
-		//const gearAL = gwHelper.actionLine(gp1, gp2, initAngle1, acc, d12, param['gw2Position']);
-		//gearAL.prepare();
-		//for (const laCtr of gearAL.getContours()) {
-		//	figOne.addDynamics(laCtr);
-		//}
-		//figOne.addPoints(gearAL.getContactPoint());
-		//gp2.set6Angles(gearAL.getInitAngle2(), acc + Math.PI);
-		//rGeome.logstr += gearAL.getMsg();
-		gp1.set7InvoluteDetails(param2.involArcPairs1, param2.skinThickness1);
-		//gp2.set7InvoluteDetails(param['involArcPairs2'], param['skinThickness2']);
-		// construction lines and circles
-		for (const refCircle of gp1.getRefCircles()) {
-			figOne.addDynamics(refCircle);
-		}
-		figOne.addDynamics(gp1.getToothRef());
-		//for (const refCircle of gp2.getRefCircles()) {
-		//	figOne.addDynamics(refCircle);
-		//}
-		//figOne.addDynamics(gp2.getToothRef());
-		// gearwheel-1
-		const gp1p = gp1.getProfile();
-		rGeome.logstr += gp1.getMsg();
-		rGeome.logstr += gp1p.check();
-		figOne.addMain(gp1p);
-		if (param.centralAxis === 1) {
-			const g1axis = welem.axisTorque(
-				gp1.cx,
-				gp1.cy,
-				param.axisRadius,
-				param.ribNb,
-				param.ribWidth,
-				param.ribHeight,
-				param.ribRound1,
-				param.ribRound2,
-				initAngle1
-			);
-			rGeome.logstr += g1axis.check();
-			figOne.addMain(g1axis);
-		}
-		if (param.hollow === 1) {
-			const materialHeightExtMax = gp1.br;
-			const materialHeightIntMin = param.axisRadius + param.ribHeight;
-			const hollowMaterialExt = materialHeightExtMax - param.materialHeightExt;
-			const hollowMaterialInt = materialHeightIntMin + param.materialHeightInt;
-			if (hollowMaterialInt > hollowMaterialExt) {
-				throw `err902: hollowMaterialInt ${ffix(
-					hollowMaterialInt
-				)} bigger than hollowMaterialExt ${ffix(hollowMaterialExt)}`;
-			}
-			const g1hollow = welem.hollowStraight(
-				gp1.cx,
-				gp1.cy,
-				hollowMaterialExt,
-				hollowMaterialInt,
-				param.spokeNb,
-				param.spokeWidth,
-				param.spokeRound,
-				initAngle1
-			);
-			for (const g1hollowE of g1hollow) {
-				rGeome.logstr += g1hollowE.check();
-				figOne.addMain(g1hollowE);
-			}
-		}
-		//const gp2p = gp2.getProfile();
-		//rGeome.logstr += gp2p.check();
-		//figOne.addSecond(gp2p);
-		// Figure Two
-		const wheelRadius = gp1.ar + param.wheelRadiusExtra;
-		if (param.wheelAxis === 1) {
-			const ctrAxisProfile_right = welem.axisProfile(
-				param.wheelHeight,
-				param.wheelMidExtra,
-				param.wheelAxisLength,
-				param.wheelAxisRadius,
-				param.wheelMidRadius,
-				wheelRadius,
-				param.wheelAxisExtRound,
-				param.wheelAxisIntRound,
-				param.wheelExtraRound,
-				true
-			);
-			const ctrAxisProfile_left = welem.axisProfile(
-				param.wheelHeight,
-				param.wheelMidExtra,
-				param.wheelAxisLength,
-				param.wheelAxisRadius,
-				param.wheelMidRadius,
-				wheelRadius,
-				param.wheelAxisExtRound,
-				param.wheelAxisIntRound,
-				param.wheelExtraRound,
-				false
-			);
-			figTwo.addMain(ctrAxisProfile_right);
-			figTwo.addSecond(ctrAxisProfile_left);
-		} else {
-			const ctrAxisProfile_right = contour(0, -param.wheelHeight / 2)
-				.addSegStrokeR(0, param.wheelHeight)
-				.addSegStrokeR(wheelRadius, 0)
-				.addSegStrokeR(0, -param.wheelHeight)
-				.closeSegStroke();
-			const ctrAxisProfile_left = contour(0, -param.wheelHeight / 2)
-				.addSegStrokeR(0, param.wheelHeight)
-				.addSegStrokeR(-wheelRadius, 0)
-				.addSegStrokeR(0, -param.wheelHeight)
-				.closeSegStroke();
-			figTwo.addMain(ctrAxisProfile_right);
-			figTwo.addSecond(ctrAxisProfile_left);
-		}
-		rGeome.fig = { teethProfile: figOne, axisProfile: figTwo };
-		const designName = rGeome.partName;
-		const axisHLength =
-			param.wheelHeight / 2 + param.wheelMidExtra + param.wheelAxisLength + 10;
-		rGeome.vol = {
-			extrudes: [
-				{
-					outName: `subpax_${designName}_teethProfile`,
-					face: `${designName}_teethProfile`,
-					extrudeMethod: EExtrude.eLinearOrtho,
-					length: 2 * axisHLength,
-					rotate: [0, 0, 0],
-					translate: [0, 0, -axisHLength]
-				},
-				{
-					outName: `subpax_${designName}_axisProfile`,
-					face: `${designName}_axisProfile`,
-					extrudeMethod: EExtrude.eRotate,
-					rotate: [0, 0, 0],
-					translate: [0, 0, 0]
-				}
-			],
-			volumes: [
-				{
-					outName: `pax_${designName}`,
-					boolMethod: EBVolume.eIntersection,
-					inList: [
-						`subpax_${designName}_teethProfile`,
-						`subpax_${designName}_axisProfile`
-					]
-				}
-			]
-		};
-		// sub-design
+		// sub-design gear_wheel_wheel
 		const gearWWParam = designParam(gearWheelWheelDef.pDef);
 		gearWWParam.setVal('module', param.module);
 		gearWWParam.setVal('N1', param.N1);
@@ -386,6 +185,35 @@ function pGeom(t: number, param: tParamVal): tGeom {
 		gearWWParam.setVal('wheelAxisExtRound', param.wheelAxisExtRound);
 		gearWWParam.setVal('wheelAxisIntRound', param.wheelAxisIntRound);
 		gearWWParam.setVal('wheelExtraRound', param.wheelExtraRound);
+		const gearWWGeom = gearWheelWheelDef.pGeom(0, gearWWParam.getParamVal());
+		checkGeom(gearWWGeom);
+		rGeome.logstr += prefixLog(gearWWGeom.logstr, gearWWParam.partName);
+		// figures
+		figOne.mergeFigure(gearWWGeom.fig.figOne);
+		figTwo.mergeFigure(gearWWGeom.fig.figTwo);
+		// final figure list
+		rGeome.fig = { teethProfile: figOne, axisProfile: figTwo };
+		const designName = rGeome.partName;
+		rGeome.vol = {
+			inherits: [
+				{
+					outName: `inpax_${designName}_gearWW`,
+					subdesign: 'pax_gear_wheel_wheel',
+					subgeom: gearWWGeom,
+					rotate: [0, 0, 0],
+					translate: [0, 0, 0]
+				}
+			],
+			extrudes: [],
+			volumes: [
+				{
+					outName: `pax_${designName}`,
+					boolMethod: EBVolume.eIdentity,
+					inList: [`inpax_${designName}_gearWW`]
+				}
+			]
+		};
+		// sub-design
 		const subGearWW: tSubInst = {
 			partName: gearWWParam.getPartName(),
 			dparam: gearWWParam.getDesignParamList(),
