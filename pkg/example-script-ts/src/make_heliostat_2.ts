e371e69c74f22a0c3cd27c76eee44a9661efc983
charles <charles.braquet@e.email>
2023-12-10 11:22:09 +0100

    adding all parameters of heliostat_2

--- pkg/example-script-ts/src/make_heliostat_2.ts
+++ pkg/example-script-ts/src/make_heliostat_2.ts
@@ -9,8 +9,33 @@ function make_heliostat_2() {
 	let logstr = '';
 	try {
 		const helioParam = designParam(heliostat_2Def.pDef);
-		helioParam.setVal('H1', 2000);
-		helioParam.setVal('H2', 2000);
+		helioParam.setVal('H1', 3000); // 3000 mm
+		helioParam.setVal('H2', 2500); // 2500 mm
+		helioParam.setVal('H3', 200); // 200 mm
+		helioParam.setVal('H4', 800); // 800 mm
+		helioParam.setVal('H5', 3000); // 3000 mm
+		helioParam.setVal('H6', 200); // 200 mm
+		helioParam.setVal('H7', 400); // 400 mm
+		helioParam.setVal('H9', 100); // 100 mm
+		helioParam.setVal('D1', 1000); // 1000 mm
+		helioParam.setVal('D2', 700); // 700 mm
+		helioParam.setVal('D3', 900); // 900 mm
+		helioParam.setVal('D4', 400); // 400 mm
+		helioParam.setVal('D5', 300); // 300 mm
+		helioParam.setVal('D6', 200); // 200 mm
+		helioParam.setVal('D7', 400); // 400 mm
+		helioParam.setVal('D9', 100); // 100 mm
+		helioParam.setVal('E1', 30); // 30 mm
+		helioParam.setVal('L1', 12500); // 12500 mm
+		helioParam.setVal('L2', 6000); // 6000 mm
+		helioParam.setVal('L3', 100); // 100 mm
+		helioParam.setVal('L4', 600); // 600 mm
+		helioParam.setVal('L5', 2000); // 2000 mm
+		helioParam.setVal('L6', 2000); // 2000 mm
+		helioParam.setVal('L7', 100); // 100 mm
+		helioParam.setVal('L8', 200); // 200 mm
+		helioParam.setVal('al', 80); // 80 degree
+		helioParam.setVal('S1', 100); // 100 mm
 		const helioGeom = heliostat_2Def.pGeom(simtime, helioParam.getParamVal());
 		checkGeom(helioGeom);
 		logstr += prefixLog(helioGeom.logstr, helioParam.partName);
