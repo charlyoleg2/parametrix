10185f43de25fdb657cc3ff990dd5d8b588c7ed6
charles <charles.braquet@e.email>
2023-12-12 22:15:32 +0100

    fixing script-js

--- pkg/designix-script-js/.eslintrc.cjs
+++ pkg/designix-script-js/.eslintrc.cjs
@@ -1,24 +1,15 @@
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended-type-checked',
-		//'plugin:@typescript-eslint/strict-type-checked',
-		'plugin:@typescript-eslint/stylistic-type-checked',
-		'prettier'
-	],
-	plugins: ['@typescript-eslint'],
-	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'prettier'],
+	plugins: [],
 	parserOptions: {
-		//sourceType: 'module',
-		//ecmaVersion: 2020,
-		project: true,
-		tsconfigRootDir: __dirname
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	env: {
+		// es2021: true,
+		// node: true,
+		browser: true
 	},
 	root: true
-	//env: {
-	//	browser: true,
-	//	es2021: true,
-	//	node: true
-	//},
 	//ignorePatterns: ['*.cjs'],
 };
