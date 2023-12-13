d55fd1048018ae4c3d18f3e5211dcad511f0d9e4
charles <charles.braquet@e.email>
2023-12-13 20:54:37 +0100

    fix ci

--- pkg/geomcli/src/geom_cli.ts
+++ pkg/geomcli/src/geom_cli.ts
@@ -5,6 +5,7 @@ import type { tAllPageDef } from 'geometrix';
 function geom_cli(iArgs: string[], dList: tAllPageDef, outDir = 'output') {
 	console.log(iArgs[0]);
 	console.log(Object.keys(dList)[0]);
+	console.log(outDir);
 }
 
 export { geom_cli };
