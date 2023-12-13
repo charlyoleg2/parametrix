eece10d2980df35d78f779b19f73c7bc369b9831
charles <charles.braquet@e.email>
2023-12-13 21:14:36 +0100

    cli based on yargs

--- pkg/geomcli/src/geom_cli.ts
+++ pkg/geomcli/src/geom_cli.ts
@@ -1,9 +1,22 @@
 // geom_cli.ts
 
 import type { tAllPageDef } from 'geometrix';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 function geom_cli(iArgs: string[], dList: tAllPageDef, outDir = 'output') {
-	console.log(iArgs[0]);
+	const argv = yargs(hideBin(iArgs))
+		.scriptName('geom_cli')
+		.usage('Usage: $0 options')
+		.option('discoverDir', {
+			alias: 'd',
+			type: 'string',
+			description: 'directory-path for searching git-repositories.',
+			default: '.'
+		})
+		.strict()
+		.parseSync();
+	console.log(argv.discoverDir);
 	console.log(Object.keys(dList)[0]);
 	console.log(outDir);
 }
