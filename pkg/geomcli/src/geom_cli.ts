ce8989e536c3aefec9d82895d712e6b3562fcc55
charles <charles.braquet@e.email>
2023-12-13 22:31:44 +0100

    working on cli

--- pkg/geomcli/src/geom_cli.ts
+++ pkg/geomcli/src/geom_cli.ts
@@ -4,19 +4,30 @@ import type { tAllPageDef } from 'geometrix';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
+function display_design_list(dList: tAllPageDef) {
+	console.log('List of available designs:');
+	for (const [idx, dname] of Object.keys(dList).entries()) {
+		console.log(`${idx.toString().padStart(4, ' ')} : ${dname}`);
+	}
+}
+
 function geom_cli(iArgs: string[], dList: tAllPageDef, outDir = 'output') {
 	const argv = yargs(hideBin(iArgs))
 		.scriptName('geom_cli')
-		.usage('Usage: $0 options')
-		.option('discoverDir', {
+		.usage('Usage: $0 <global-options> command <command-options>')
+		.option('design', {
 			alias: 'd',
 			type: 'string',
-			description: 'directory-path for searching git-repositories.',
-			default: '.'
+			description: 'design to be used by the command',
+			default: 'aaa'
+		})
+		.command('list', 'list the available designs', {}, () => {
+			display_design_list(dList);
 		})
 		.strict()
 		.parseSync();
-	console.log(argv.discoverDir);
+	console.log(argv.$0);
+	console.log(argv.design);
 	console.log(Object.keys(dList)[0]);
 	console.log(outDir);
 }
