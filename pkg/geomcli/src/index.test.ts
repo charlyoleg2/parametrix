7daa6c472c43ac8dbe4062cb96a76ad1c10d1440
charles <charles.braquet@e.email>
2023-12-15 22:57:35 +0100

    more interedting unit test

--- pkg/geomcli/src/index.test.ts
+++ pkg/geomcli/src/index.test.ts
@@ -1,8 +1,16 @@
 import { describe, it, expect } from 'vitest';
-//import { geom_write } from './index';
+import { geom_cli } from './index';
+import type { tAllPageDef } from 'geometrix';
+
+import { heliostatDef, swingDef } from 'designix';
+
+const designList: tAllPageDef = {
+	'heliostat/heliostat': heliostatDef,
+	'heliostat/swing': swingDef
+};
 
 describe('geomcli index suit', () => {
-	it('geom_write', () => {
-		expect(2 + 2).toBe(4);
+	it('geom_cli', async () => {
+		await expect(geom_cli(['node', 'dummy', 'list'], designList)).resolves.toBeUndefined();
 	});
 });
