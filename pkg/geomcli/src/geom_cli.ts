800d93cf78bba938b8d7690a57b2a77d7e8ba9f9
charles <charles.braquet@e.email>
2023-12-17 04:58:23 +0100

    oops one more throw

--- pkg/geomcli/src/geom_cli.ts
+++ pkg/geomcli/src/geom_cli.ts
@@ -43,8 +43,7 @@ function parseModif(modif: string[], printLog: boolean): tParamVal {
 		const valStr = modif[2 * i + 1];
 		const val = parseFloat(valStr);
 		if (isNaN(val)) {
-			console.log(`err908: ${valStr} is not a number!`);
-			process.exit(1);
+			throw(`err908: ${valStr} is not a number!`);
 		}
 		pVal[modif[2 * i]] = val;
 	}
