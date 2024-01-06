5aad437d85a3d6da21f169227c75622e53715cd1
charles <charles.braquet@e.email>
2024-01-06 23:09:59 +0100

    fewer svg to be bundled

--- pkg/designix/scr/rm_big_svg.sh
+++ pkg/designix/scr/rm_big_svg.sh
@@ -2,6 +2,8 @@
 # rm_big_svg.sh
 
 # gears
+rm src/gears/svgo/gears.svg
+rm src/gears/svgo/involute_of_circle.svg
 
 # heliostat
 rm src/heliostat/svgo/base.svg
@@ -13,3 +15,14 @@ rm src/heliostat/svgo/swing.svg
 rm src/heliostat/svgo/rod.svg
 rm src/heliostat/svgo/trapeze.svg
 rm src/heliostat/svgo/surface.svg
+
+# dummy
+rm src/dummy/svgo/circles.svg
+rm src/dummy/svgo/rectangle.svg
+rm src/dummy/svgo/dummy_pole_static.svg
+
+# geometrix_verification
+rm src/geometrix_verification/svgo/verify_contour_1.svg
+rm src/geometrix_verification/svgo/verify_line.svg
+rm src/geometrix_verification/svgo/verify_point.svg
+
