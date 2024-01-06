894fc1164dcac3affeb60dc632da689a65ea0b5d
charles <charles.braquet@e.email>
2024-01-06 22:39:58 +0100

    prune svgo

---
+++ pkg/designix/scr/rm_big_svg.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# rm_big_svg.sh
+
+# gears
+
+# heliostat
+rm src/heliostat/svgo/base.svg
+rm src/heliostat/svgo/heliostat.svg
+rm src/heliostat/svgo/pole_stator.svg
+rm src/heliostat/svgo/pole_rotor.svg
+rm src/heliostat/svgo/rake.svg
+rm src/heliostat/svgo/swing.svg
+rm src/heliostat/svgo/rod.svg
+rm src/heliostat/svgo/trapeze.svg
+rm src/heliostat/svgo/surface.svg
