b4752db4eefaf38bd902c4d99733b67d1fb3dec0
charles <charles.braquet@e.email>
2024-01-07 23:52:07 +0100

    read version from package.json

--- pkg/paramui/src/lib/detailed_versions.ts
+++ pkg/paramui/src/lib/detailed_versions.ts
@@ -1,16 +1,7 @@
 // detailes_versions.ts
 
 import { version_details } from 'geometrix';
-//import appPackage from '../../../package.json';
-
-const appPackage = {
-	name: 'appAbc',
-	version: '1.2.3',
-	dependencies: {
-		babaorum: '^2.3.4',
-		petibonum: '^9.8.7'
-	}
-};
+import appPackage from '../../package.json';
 
 const detailed_versions = version_details(appPackage);
 
