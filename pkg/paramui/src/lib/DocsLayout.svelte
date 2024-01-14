2486092dc591932eebdd34a3a9407580bef22777
charles <charles.braquet@e.email>
2024-01-14 12:56:41 +0100

    unified docs layout

---
+++ pkg/paramui/src/lib/DocsLayout.svelte
@@ -0,0 +1,12 @@
+<article>
+	<slot>
+		<!-- the mdsvex content will be slotted in here -->
+	</slot>
+</article>
+
+<style lang="scss">
+	/*@use '$lib/style/colors.scss';*/
+	article {
+		margin: 1rem;
+	}
+</style>
--- pkg/paramui/svelte.config.js
+++ pkg/paramui/svelte.config.js
@@ -1,15 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import { join } from 'path';
 
 const dev = process.argv.includes('dev');
+const pathToDocsLayout = join(import.meta.dirname, './src/lib/DocsLayout.svelte');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.svx', '.md'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.svx', '.md'] })],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.svx', '.md'],
+			layout: pathToDocsLayout
+		})
+	],
 
 	kit: {
 		adapter: adapter({
