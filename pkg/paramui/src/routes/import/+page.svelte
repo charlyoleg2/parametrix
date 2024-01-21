7862b60d98196a04dc01bb9dafb8ade55b164342
charles <charles.braquet@e.email>
2024-01-21 21:12:37 +0100

    dynamic import can not be intercepted by service worker

--- pkg/paramui/src/routes/import/+page.svelte
+++ pkg/paramui/src/routes/import/+page.svelte
@@ -69,6 +69,10 @@
 		} else {
 			downloadMsg += 'err565: Error by downloading\n';
 		}
+		// import code
+		const impObg = await import(`${base}/impDesign5432.js`);
+		downloadMsg += `dbg320: import code\n`;
+		impObg.abc1();
 	}
 </script>
 
