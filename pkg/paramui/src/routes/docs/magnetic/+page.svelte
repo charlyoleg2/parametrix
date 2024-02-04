7e8ef6ae22ce6e797970bd8be38e8adbe27bf07b
charles <charles.braquet@e.email>
2024-02-04 22:18:00 +0100

    fixing svg path 2

--- pkg/paramui/src/routes/docs/magnetic/+page.svelte
+++ pkg/paramui/src/routes/docs/magnetic/+page.svelte
@@ -508,7 +508,7 @@
 </article>
 <h3>Regular torus</h3>
 <article class="splitable">
-	<img src="{base}/regular_torus.svg" alt="regular torus" />
+	<img src="{base}/psvg/regular_torus.svg" alt="regular torus" />
 	<ul class="formula">
 		<li>{@html math('L = 2 \\pi R')} (length of the torus)</li>
 		<li>{@html math('\\mathcal{F} = N i')}</li>
@@ -648,7 +648,7 @@
 </article>
 <h3>Torus with swelling</h3>
 <article class="splitable">
-	<img src="{base}/torus_with_swelling.svg" alt="torus with swelling" />
+	<img src="{base}/psvg/torus_with_swelling.svg" alt="torus with swelling" />
 	<ul class="formula">
 		<li>{@html math('\\mathcal{F} = N i')}</li>
 		<li>
@@ -763,7 +763,7 @@
 </article>
 <h3>Torus with air gap</h3>
 <article class="splitable">
-	<img src="{base}/torus_with_air_gap.svg" alt="torus with air gap" />
+	<img src="{base}/psvg/torus_with_air_gap.svg" alt="torus with air gap" />
 	<ul class="formula">
 		<li>{@html math('\\mathcal{F} = N i')}</li>
 		<li>
@@ -842,8 +842,8 @@
 </article>
 <h3>Torus with shuttle</h3>
 <article class="splitable">
-	<img src="{base}/torus_with_shuttle.svg" alt="torus with shuttle" />
-	<img src="{base}/top_view_of_air_gap_and_shuttle.svg" alt="top view of air gap and shuttle" />
+	<img src="{base}/psvg/torus_with_shuttle.svg" alt="torus with shuttle" />
+	<img src="{base}/psvg/top_view_of_air_gap_and_shuttle.svg" alt="top view of air gap and shuttle" />
 	<ul class="formula">
 		<li>{@html math('\\mathcal{F} = N i')}</li>
 		<li>
@@ -1013,7 +1013,7 @@
 </article>
 <h3>Torus with realistic shuttle</h3>
 <article class="splitable">
-	<img src="{base}/torus_with_realistic_shuttle.svg" alt="torus with a realistic shuttle" />
+	<img src="{base}/psvg/torus_with_realistic_shuttle.svg" alt="torus with a realistic shuttle" />
 	<ul class="formula">
 		<li>{@html math('\\mathcal{F} = N i')}</li>
 		<li>
