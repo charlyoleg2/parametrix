045510c0b5d489349c8eb8cd0e93545b561ab700
charles <charles.braquet@e.email>
2024-02-07 20:11:04 +0100

    more docs pages

---
+++ pkg/paramui/src/routes/docs/prez/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	//import { base } from '$app/paths';
+</script>
+
+<h1>Prez of Parametrix</h1>
+<article>Some slides that present Parametrix.</article>
+
+<style lang="scss">
+	@use '$lib/style/colors.scss';
+
+	h1 {
+		margin: 1rem;
+	}
+	article {
+		margin: 1rem;
+	}
+</style>
