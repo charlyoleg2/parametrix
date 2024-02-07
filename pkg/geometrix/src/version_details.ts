2ed9a00223c1c226ea02d5253cfadcc0a83d3bbf
charles <charles.braquet@e.email>
2024-02-07 20:58:50 +0100

    links to api rel external

--- pkg/geometrix/src/version_details.ts
+++ pkg/geometrix/src/version_details.ts
@@ -1,12 +1,22 @@
 // version_details.ts
 
+/**
+ * For cli-app and ui-app
+ */
 type tDependencies = Record<string, string>; // package-name: package-version
+
+/**
+ * For cli-app and ui-app
+ */
 interface tPackage {
 	name: string;
 	version: string;
 	dependencies: tDependencies;
 }
 
+/**
+ * For cli-app and ui-app
+ */
 function version_details(appPackage: tPackage): string[] {
 	//const lb = html ? '<br>' : '\n'; // line-break
 	const rStr: string[] = [];
