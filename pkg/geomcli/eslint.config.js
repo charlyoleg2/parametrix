5cc9f2ea516d1808cfdf63d6fb4cbf5cf6830ab1
charlyoleg <charles.braquet@gmx.fr>
2024-08-03 17:00:26 +0200

    geomcli upgraded to eslint v9

---
+++ pkg/geomcli/eslint.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	{
+		ignores: ['dist/', 'build/', 'node_modules/']
+	},
+	eslint.configs.recommended,
+	//...tseslint.configs.recommended,
+	...tseslint.configs.strict,
+	...tseslint.configs.stylistic
+);
