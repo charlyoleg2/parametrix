cf7266a5b9783f413fee088eec7b9146ab35fc0c
charlyoleg <charles.braquet@gmx.fr>
2025-01-19 21:22:12 +0100

    intro completed

--- pkg/paramui/src/routes/docs/intro/+page.svelte
+++ pkg/paramui/src/routes/docs/intro/+page.svelte
@@ -13,6 +13,41 @@
 		</div>
 	</OneSlide>
 	<OneSlide>
+		<pre style="font-size:2.5em; margin:0;">Parametrix</pre>
+		<pre style="font-size:2em; margin:1em;">CAD as code</pre>
+		<pre class="nomar">&gt; Git friendly</pre>
+		<pre class="nomar">&gt;&gt; traceability, history, versioning</pre>
+		<pre class="nomar">&gt;&gt; collaboration, sharing, backup</pre>
+		<pre class="nomar">&gt; CAD developed like software</pre>
+		<pre class="nomar">&gt;&gt; CI/CD</pre>
+	</OneSlide>
+	<OneSlide>
+		<pre style="font-size:2.5em; margin:0;">Parametrix</pre>
+		<pre style="font-size:2em; margin:1em;">CAD as code</pre>
+		<pre style="font-size:1.2em; color:Orange;">Yes, but all CAD tools have an API!</pre>
+	</OneSlide>
+	<OneSlide>
+		<pre style="font-size:1em; color:Green;">Indeed, but 3 main advantages with Parametrix</pre>
+		<pre class="point">1- Written in Modern Javascript</pre>
+		<pre class="blabla">Run in CLI and Browser</pre>
+		<pre class="blabla">Design easy to share via websites and packages</pre>
+		<pre class="point">2- Durability of your 3D modeling</pre>
+		<pre class="blabla">Parametrix is Open source and lightweight</pre>
+		<pre class="blabla">(geometrix: 6k, geomui: 3k, geomcli: 1k)</pre>
+		<pre class="point">3- Independance from CAD tools</pre>
+		<pre class="blabla">Model first then choose your CAD tool</pre>
+	</OneSlide>
+	<OneSlide>
+		<pre style="font-size:1em; color:Green;">Plus other benefits</pre>
+		<pre class="point">4- Documented parameters</pre>
+		<pre class="point">5- Make easily your Web-UI and CLI</pre>
+		<pre class="blabla">Just in few lines of code</pre>
+		<pre class="point">6- 2D simulations</pre>
+		<pre class="blabla">Examples for kinetic and optic simulations</pre>
+		<pre class="point">7- Extendable API via npm-package</pre>
+		<pre class="blabla">API for metal sheet, API for architecture</pre>
+	</OneSlide>
+	<OneSlide>
 		<img
 			src="{base}/puisvg/intro_ecosystem.svg"
 			style="width: 28em;"
@@ -27,5 +62,16 @@
 
 	h1 {
 		margin: 1rem;
+	}
+	pre.nomar {
+		margin: 0;
+	}
+	pre.point {
+		font-size: 1.4em;
+		margin: 1em 0 0.2em;
+	}
+	pre.blabla {
+		font-size: 1em;
+		margin: 0 3em 0;
 	}
 </style>
