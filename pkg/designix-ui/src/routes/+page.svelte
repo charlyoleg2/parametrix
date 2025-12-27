b979fd2a391fcf3e4b80ab58de893cbee8559514
charlyoleg <charles.braquet@gmx.fr>
2025-12-27 06:23:00 +0100

    designix-ui moves to resolve

--- pkg/designix-ui/src/routes/+page.svelte
+++ pkg/designix-ui/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { designNameList } from '$lib/makeList';
-	import { base } from '$app/paths';
+	import { resolve } from '$app/paths';
 </script>
 
 <h2>List of designs:</h2>
 <ol>
 	{#each designNameList as designOne (designOne)}
-		<li><a href={`${base}/${designOne}`}>{designOne}</a></li>
+		<li><a href={resolve(`/${designOne}`)}>{designOne}</a></li>
 	{/each}
 </ol>
 
